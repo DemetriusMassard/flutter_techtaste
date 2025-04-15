b2160a5c2adc3531a446db15fedb77611f94f8b5
Demetrius Massard <demetrius2597@hotmail.com>
2025-04-14 21:13:45 -0300

    feat(splash_screen): Splash screen creation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'splash/splash_screen.dart';
+import '_core/app_theme.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SplashScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.appTheme,
+      home: const SplashScreen(),
+    );
   }
 }
--- lib/app/splash/splash_screen.dart
+++ lib/app/splash/splash_screen.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
 
+import '../_core/app_colors.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Image.asset(
+              'lib/assets/banners/Banner.png',
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Center(
+            child: Column(
+              spacing: 32.0,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'lib/assets/Logo home.png',
+                ),
+                const Column(
+                  children: [
+                    Text(
+                      "Um parceiro inovador para sua",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                    Text(
+                      "Melhor experiência culinária!",
+                      style: TextStyle(
+                          color: AppColors.mainColor,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text("Bora!"),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: AppColors.backgroundColor,
+    );
   }
 }
