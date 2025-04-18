aa8ab17f9a63b9a5e405447df4cd546550a92f47
Demetrius Massard <demetrius2597@hotmail.com>
2025-04-17 22:40:08 -0300

    feat(restaurant)Restaurant screen redesign

--- lib/app/restaurant/restaurant_screen.dart
+++ lib/app/restaurant/restaurant_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_techtaste/app/_core/app_colors.dart';
-import 'package:flutter_techtaste/app/_core/bag_provider.dart';
 import 'package:flutter_techtaste/app/_core/widgets/appbar.dart';
+import 'package:flutter_techtaste/app/restaurant/widgets/dish_widget.dart';
 import 'package:flutter_techtaste/model/restaurant.dart';
-import 'package:provider/provider.dart';
 
 import '../../model/dish.dart';
 
@@ -13,53 +12,59 @@ class RestaurantScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: getAppbar(context: context, title: restaurant.name),
-        body: Padding(
-          padding: const EdgeInsets.all(32.0),
-          child: Center(
-            child: Column(
-              spacing: 12,
-              children: [
-                Image.asset(
-                  "lib/assets/${restaurant.imagePath}",
-                  width: 160,
-                  fit: BoxFit.fill,
+      appBar: getAppbar(context: context, title: restaurant.name),
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            spacing: 12,
+            children: [
+              Image.asset(
+                "lib/assets/${restaurant.imagePath}",
+                width: 160,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: Text(
+                  "Mais pedidos",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: AppColors.mainTextColor),
                 ),
-                SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    "Mais pedidos",
-                    style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
-                        color: AppColors.mainTextColor),
+              ),
+              Flexible(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    spacing: 16,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: List.generate(
+                      restaurant.dishes.length,
+                      (index) {
+                        Dish dish = restaurant.dishes[index];
+                        // return ListTile(
+                        //   leading: _getDishImage(dish.imagePath),
+                        //   title: Text(dish.name),
+                        //   subtitle: Text("R\$ ${dish.price.toStringAsFixed(2)}"),
+                        //   trailing: IconButton(
+                        //     onPressed: () {
+                        //       context.read<BagProvider>().addAllDishes([dish]);
+                        //     },
+                        //     icon: Icon(Icons.add),
+                        //   ),
+                        // );
+                        return DishWidget(dish: dish);
+                      },
+                    ),
                   ),
                 ),
-                Column(
-                  children: List.generate(restaurant.dishes.length, (index) {
-                    Dish dish = restaurant.dishes[index];
-                    return ListTile(
-                      leading: _getDishImage(dish.imagePath),
-                      title: Text(dish.name),
-                      subtitle: Text("R\$ ${dish.price.toStringAsFixed(2)}"),
-                      trailing: IconButton(
-                        onPressed: () {
-                          context.read<BagProvider>().addAllDishes([dish]);
-                        },
-                        icon: Icon(Icons.add),
-                      ),
-                    );
-                  }),
-                )
-              ],
-            ),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
-}
-
-Image _getDishImage(String imagePath) {
-  Image imjObj;
-  imjObj = Image.asset('lib/assets/dishes/Salada (1).png');
-  return imjObj;
 }
---
+++ lib/app/restaurant/widgets/dish_widget.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_techtaste/model/dish.dart';
+
+import '../../_core/app_colors.dart';
+
+class DishWidget extends StatelessWidget {
+  final Dish dish;
+  const DishWidget({super.key, required this.dish});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        height: 292,
+        decoration: BoxDecoration(color: AppColors.lightBackgroundColor),
+        child: Column(
+          spacing: 12,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 160,
+              child: Image.asset(
+                'lib/assets/dishes/Salada (1).png',
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    dish.name,
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      color: AppColors.mainTextColor,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    "R\$ ${dish.price.toStringAsFixed(2)}",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      color: AppColors.secondaryTextColor,
+                      fontSize: 14,
+                    ),
+                  ),
+                  SizedBox(height: 14),
+                  Text(
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    dish.description,
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      color: AppColors.secondaryTextColor,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
